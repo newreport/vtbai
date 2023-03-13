d14d27f354dc5a066b899adb40b6c6260d5d25e1
newreport <haruki959@qq.com>
2023-03-13 19:08:19 +0800

    .

--- demo/pth2onnx.py
+++ demo/pth2onnx.py
@@ -1,27 +1,17 @@
+ 
+
+
 import torch
-import torch.onnx
-from tinynet import tinynet
-import os
+from light_cnns import mbv1
 
-def pth_to_onnx(input, checkpoint, onnx_path, input_names=['input'], output_names=['output'], device='cpu'):
-    if not onnx_path.endswith('.onnx'):
-        print('Warning! The onnx model name is not correct,\
-              please give a name that ends with \'.onnx\'!')
-        return 0
-
-    model = tinynet() #导入模型
-    model.load_state_dict(torch.load(checkpoint)) #初始化权重
-    model.eval()
-    # model.to(device)
-    
-    torch.onnx.export(model, input, onnx_path, verbose=True, input_names=input_names, output_names=output_names) #指定模型的输入，以及onnx的输出路径
-    print("Exporting .pth model to onnx model has been successful!")
-
+sourcePath='/app/live_vits_chatgpt/models/G_latest.pth'
+outPath='/app/live_vits_chatgpt/models/G_latest.onnx'
+ 
+def main():
+    model = mbv1()
+    imput = torch.randn(1,3,224,224)
+    torch.onnx.export(model, input, "mobilenetV2.onnx", verbose=True, opset_version=11, export_params=True)
+ 
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES']='2'
-    checkpoint = './tinynet.pth'
-    onnx_path = './tinynet.onnx'
-    input = torch.randn(1, 1, 640, 360)
-    # device = torch.device("cuda:2" if torch.cuda.is_available() else 'cpu')
-    pth_to_onnx(input, checkpoint, onnx_path)
+    main()
 
