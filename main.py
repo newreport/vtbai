ac7c334ef641e4b90226c40fabe28b4138364ddf
newreport <haruki959@qq.com>
2023-03-14 19:21:47 +0800

    .

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import openai
-from bilibili_api import live, sync
+import asyncio
+import random
+import blivedm.blivedm as blivedm
 import configparser
 from queue import Queue, PriorityQueue
 import json
@@ -32,8 +34,11 @@ def write_excel_xls_append(value):
     new_worksheet = new_workbook.get_sheet(sheetName)  # 获取转化后工作簿中的第一个表格
     new_worksheet.write(rows_old, 0, value['datetime'])
     new_worksheet.write(rows_old, 1, value['user'])
-    new_worksheet.write(rows_old, 2, value['action'])
-    new_worksheet.write(rows_old, 3, value['msg'])
+    new_worksheet.write(rows_old, 2, value['type'])
+    new_worksheet.write(rows_old, 3, value['num'])
+    new_worksheet.write(rows_old, 4, value['action'])
+    new_worksheet.write(rows_old, 5, value['msg'])
+    new_worksheet.write(rows_old, 6, value['price'])
     new_workbook.save(xlslPATH)  # 保存工作簿
     if mainConfig['env'] == 'dev':
         print("xls格式表格【追加】写入数据成功！")
@@ -52,28 +57,53 @@ def write_keyboard_text(text):
 
 
 def send2gpt(msg):
+    return
     if mainConfig['env'] == 'dev':
         print('gpt当前进程id::'+str(os.getpid()))
 
     # 向gpt发送消息
-    sendGptMsg = msg['name']+msg['action']+msg['msg']
+    sendGptMsg = ''
+    sendVitsMsg = ''
+    if msg['type'] == 'danmu':
+        sendGptMsg = msg['name']+msg['action']+msg['msg']
+        sendVitsMsg = msg['msg']
+    elif msg['type'] == 'sc':
+        sendGptMsg = msg['name']+msg['action']+msg['price']+'块钱sc说'+msg['msg']
+        sendVitsMsg = sendGptMsg
+    elif msg['type'] == 'guard':
+        guardType = '舰长'
+        if msg['price'] > 200:
+            guardType = '提督'
+        elif msg['price'] > 2000:
+            guardType = '总督'
+        sendGptMsg = msg['name']+msg['action'] + \
+            guardType+'了,花了'+msg['price']+'元'
+        sendVitsMsg = msg['name']+msg['action'] + guardType+'了'
+    elif msg['type'] == 'gift':
+        sendGptMsg = msg['name']+msg['action'] + \
+            msg['msg']+',花了'+msg['price']+'元'
+        sendVitsMsg = msg['name']+msg['action'] + msg['msg']
+
     print("向gpt发送::"+sendGptMsg)
-    # 对话日志
+    # 对话日志 excel
     with open('output/'+str(datetime.date.today())+'.txt', 'a') as a:
         a.write(str(datetime.datetime.now())+"::发送::"+sendGptMsg+'\n')
         a.flush()
         write_excel_xls_append({
             'datetime': str(datetime.datetime.now()),
             'user': msg['name'],
+            'type': msg['type'],
+            'num': msg['num'],
             'action': msg['action'],
-            'msg': msg['msg']
+            'msg': msg['msg'],
+            'price': msg['price']
         })
+
     # 发送给gpt
     tempMessage.append({"role": "user", "content": sendGptMsg})
     if len(tempMessage) > 3:
         del (tempMessage[0])
     message = baseContext+tempMessage
-    print(message)
 
     # 接收
     response = openai.ChatCompletion.create(
@@ -83,15 +113,15 @@ def send2gpt(msg):
         return
     print("从gpt接收::"+responseText)
 
-    # 生成发送语言
-    sendVitsMsg = msg['msg']
+    # 生成发送语音
     vits.generated_speech(sendVitsMsg, 'sendVits.wav')
-    # 生成接收语言
+    # 生成接收语音
     vits.generated_speech(responseText, 'recVits.wav')
-    write_keyboard_text(responseText)
 
     playsound('output/sendVits.wav')
     time.sleep(0.5)
+    # 模拟键盘输入
+    write_keyboard_text(responseText)
     playsound('output/recVits.wav')
 
     # 对话日志
@@ -101,33 +131,46 @@ def send2gpt(msg):
         write_excel_xls_append({
             'datetime': str(datetime.datetime.now()),
             'user': 'gpt35',
+            'type': '',
+            'num': '',
             'action': '说',
-            'msg': responseText
+            'msg': responseText,
+            'price': 0
         })
 
 
 def chatgpt35():
     print("运行gpt循环任务")
-    while isRun:
-        msgs = {"name": ''}
-        if topQue.qsize() > 0:
-            # 从队列获取信息
-            msgs = topQue.get()
-        elif giftQue.qsize() > 0:
-            msgs = giftQue.get()
-        elif scQue.qsize() > 0:
-            msgs = scQue.get()
-        elif danmuQue.qsize() > 0:
-            msgs = danmuQue.get()
+    while True:
+        chatObj = [0, {"name": '', "type": '', 'num': 0,
+                       'action': '', 'msg': '', 'price': 0}]
+        # 从队列获取信息
+        try:
+            if topQue.qsize() > 0:
+                chatObj = topQue.get(True, 1)
+            elif guardQue.qsize() > 0:
+                chatObj = guardQue.get(True, 1)
+            elif giftQue.qsize() > 0:
+                chatObj = giftQue.get(True, 1)
+            elif scQue.qsize() > 0:
+                chatObj = scQue.get(True, 1)
+            elif danmuQue.qsize() > 0:
+                chatObj = danmuQue.get(True, 1)
+            chatObj = chatObj[1]
+        except:
+            time.sleep(0)
+            continue
         # 过滤队列
-        if len(msgs['name']) > 0 and filter_text(msgs['name']) and filter_text(msgs['msg']):
-            send2gpt(msgs)
-            msgs = {"name": ''}
+        if len(chatObj['name']) > 0:
+            if filter_text(chatObj['name']) and filter_text(chatObj['msg']):
+                send2gpt(chatObj)
         else:
             time.sleep(1)
 
 
 def filter_text(text):
+    if text == '-1':
+        return True
     textPY = str.join('', lazy_pinyin(text))
     for i in range(len(hzSensitiveWord)):
         if hzSensitiveWord[i] in text or pySensitiveWord[i] in textPY:
@@ -135,12 +178,71 @@ def filter_text(text):
     return True
 
 
+async def run_single_client():
+    # 如果SSL验证失败就把ssl设为False，B站真的有过忘续证书的情况
+    client = blivedm.BLiveClient(roomID, ssl=True)
+    handler = MyHandler()
+    client.add_handler(handler)
+
+    client.start()
+    try:
+        await client.join()
+    finally:
+        await client.stop_and_close()
+
+
+class MyHandler(blivedm.BaseHandler):
+    async def _on_danmaku(self, client: blivedm.BLiveClient, message: blivedm.DanmakuMessage):
+        if message.dm_type == 0:
+            # print(f'弹幕：[{client.room_id}] {message.uname}：{message.msg}')
+            # 权重计算
+            privilege_type = message.privilege_type
+            if privilege_type == 0:
+                privilege_type = 9
+            rank = (99999-message.user_level*10+(10-privilege_type)
+                    * 10+message.mobile_verify*10)
+            print("rank:"+str(rank)+";name:"+message.uname+";msg:"+message.msg.replace('[', '').replace(']', ''))
+            danmuQue.put((rank, {'name': message.uname, 'type': 'danmu','num': 1, 'action': '说', 'msg': message.msg.replace('[', '').replace(']', ''), 'price': 0}))
+
+    async def _on_gift(self, client: blivedm.BLiveClient, message: blivedm.GiftMessage):
+        if message.coin_type == 'gold':
+            print(f'礼物：[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
+                  f' （{message.coin_type}瓜子x{message.total_coin}）')
+            price = message.total_coin/1000
+            if price > 1:
+                giftQue.put((999999-price, {"name": message.uname, "type": 'gift',
+                                            'num': message.num, 'action': message.action, 'msg': '-1', 'price': price}))
+
+    async def _on_buy_guard(self, client: blivedm.BLiveClient, message: blivedm.GuardBuyMessage):
+        print(f'上舰：[{client.room_id}] {message.username} 购买{message.gift_name}')
+        guardQue((message.guard_level, {
+            "name": message.username, "type": 'guard',
+            'num': 1, 'action': '上', 'msg': '-1', 'price': message.price/1000}))
+
+    async def _on_super_chat(self, client: blivedm.BLiveClient, message: blivedm.SuperChatMessage):
+        print(
+            f'SC:[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}')
+        # 名称、类型、数量、动作、消息、价格
+        scQue.put((999999-message.price, {"name": message.uname, "type": 'sc',
+                                          'num': 1, 'action': '发送', 'msg': message.message, 'price': message.price}))
+
+def cleanQue():
+    while True:
+        try:
+            if giftQue.full():
+                giftQue.get(True, 1)
+            elif danmuQue.full():
+                danmuQue.get(True, 1)
+        except:
+            print("满了")
+
 # 配置文件、日志、当前文本、记录excel、敏感词文本
 configINI = 'config.ini'
 tempTXT = 'output/temp.txt'
 currTXT = 'output/currText.txt'
 xlslPATH = 'output/record.xlsx'
 sensitiveTXT = 'sensitive_words.txt'
+
 # openai
 if os.path.exists('my_config.ini'):
     configINI = 'my_config.ini'
@@ -148,8 +250,8 @@ con = configparser.ConfigParser()
 con.read(configINI, encoding='utf-8')
 sections = con.sections()
 mainConfig = dict(con.items('main'))
-room = live.LiveDanmaku(json.loads(str(requests.get('https://api.live.bilibili.com/room/v1/Room/get_info?room_id=' +
-                                                    mainConfig['roomid']).content, encoding="utf-8"))['data']['room_id'])
+roomID = json.loads(str(requests.get('https://api.live.bilibili.com/room/v1/Room/get_info?room_id=' +
+                                     mainConfig['roomid']).content, encoding="utf-8"))['data']['room_id']
 openai.api_key = mainConfig['key']
 baseContext = [{"role": "system", "content": mainConfig['nya1']}]
 response = openai.ChatCompletion.create(
@@ -160,6 +262,7 @@ print("主线程发送::"+mainConfig['nya1']+"\n主线程接收::" +
 # 带基础设定最大3条上下文
 contextMessage = []
 tempMessage = []
+
 # 敏感词
 if os.path.exists('my_sensitive_words.txt'):
     sensitiveTXT = 'my_sensitive_words.txt'
@@ -169,12 +272,19 @@ pySensitiveWord = []
 for i in range(len(hzSensitiveWord)):
     hzSensitiveWord[i] = hzSensitiveWord[i].replace('\n', '')
     pySensitiveWord.append(str.join('', lazy_pinyin(hzSensitiveWord[i])))
+
 # 最优先队列、sc、礼物、弹幕队列
 topQue = Queue(maxsize=0)
+# sc 队列
 scQue = PriorityQueue(maxsize=0)
-giftQue = PriorityQueue(maxsize=10)
-danmuQue = PriorityQueue(maxsize=3)
+# 舰长队列
+guardQue = PriorityQueue(maxsize=0)
+# 礼物
+giftQue = PriorityQueue(maxsize=5)
+# 普通弹幕队列
+danmuQue = PriorityQueue(maxsize=10)
 topIDs = mainConfig['topid'].split(',')
+
 # excel数据库
 if os.path.exists(xlslPATH) == False:
     workbook = xlwt.Workbook()
@@ -184,47 +294,12 @@ if os.path.exists(xlslPATH) == False:
     print('当前进程id::'+str(os.getpid()))
 
 
-@ room.on('DANMU_MSG')
-async def on_danmaku(event):
-    if mainConfig['env'] == 'dev':
-        print('弹幕当前进程id::'+str(os.getpid()))
-    # 收到弹幕
-    name = event['data']['info'][2][1]
-    msg = event['data']['info'][1]
-    msgType = event['data']['info'][0][12]
-    if mainConfig['env'] == 'dev':
-        with open(tempTXT, 'a') as a:
-            a.write(str(event)+'\n')
-            a.flush()
-    # print('弹幕::'+str(msg))
-    if msgType == 0:
-        danmuQue.put({"name": name, "action": '说', "msg": msg})
-    elif msgType == 1:
-        print('表情::'+str(msg))
-    else:
-        print('其他::'+str(msg))
-
-
-@ room.on('SEND_GIFT')
-async def on_gift(event):
-    if mainConfig['env'] == 'dev':
-        print('礼物当前进程id::'+str(os.getpid()))
-    # 收到礼物
-    if event['data']['data']['batch_combo_send'] is not None:
-        name = event['data']['data']['batch_combo_send']['uname']
-        action = event['data']['data']['batch_combo_send']['action']
-        # num = event['data']['data']['batch_combo_send']['gift_num']
-        gift = event['data']['data']['batch_combo_send']['gift_name']
-        scQue.put({"name": name, "action": action, "msg": gift}, 9)
-        if mainConfig['env'] == 'dev':
-            with open('girf.txt', 'a') as a:
-                a.write(str(event)+'\n')
-                a.flush()
 if __name__ == '__main__':
     isRun = True
+    _thread.start_new_thread(cleanQue, ())
     _thread.start_new_thread(chatgpt35, ())
-    sync(room.connect())
-    _thread.start_new_thread(sync, (room.connect(),))
+    _thread.start_new_thread(asyncio.get_event_loop(
+    ).run_until_complete, (run_single_client(),))
     print('All subprocesses start.')
     time.sleep(2)
     input('input to exit::')
--- my_vits.py
+++ my_vits.py
@@ -23,10 +23,8 @@ def get_args():
     parser = argparse.ArgumentParser(description='inference')
     # 模型文件夹
     parser.add_argument('--onnx_model', default = './models/model.onnx')
-    parser.add_argument('--onnx2_model', default = './models/G_latest.onnx')
     # 模型配置
-    parser.add_argument('--onnx2cfg', default="./models/config.json")
-    parser.add_argument('--cfg', default="./models/moegoe_config.json")
+    parser.add_argument('--cfg', default="./models/config.json")
     # 输出目录
     parser.add_argument('--outdir', default="./output",
                         help='ouput directory')
@@ -40,13 +38,6 @@ sid = 0
 ort_sess = ort.InferenceSession(args.onnx_model)
 outdir = args.outdir
 
-
- 
-# pretrained=True就可以使用预训练的模型
-net = models.squeezenet1_1(pretrained=True)
-pthfile =open(args.pth_model,'r')
-net.load_state_dict(torch.load(pthfile))
-print(net)
 
 # text="ドルの下落"
 def generated_speech(text,fileName):
@@ -82,38 +73,8 @@ def generated_speech(text,fileName):
         # cmd = 'ffmpeg -y -i ' +  outdir + '/temp1.wav' + ' -ar 44100 '+ outdir + '/temp2.wav'
         # os.system(cmd)
 
-def generated_speech2(text,fileName):
-    text = f"[JA]{text}[JA]" if is_japanese(text) else f"[ZH]{text}[ZH]"
-    # 将文本字符串转换为id
-    seq = text_to_sequence(text, symbols=hps.symbols, cleaner_names=hps.data.text_cleaners)
-    if hps.data.add_blank:
-        seq = commons.intersperse(seq, 0)
-    with torch.no_grad():
-        x = np.array([seq], dtype=np.int64)
-        x_len = np.array([x.shape[1]], dtype=np.int64)
-        global sid 
-        sid =0
-        sid = np.array([sid], dtype=np.int64)
-        scales = np.array([0.667, 0.8, 1], dtype=np.float32)
-        scales.resize(1, 3)
-        ort_inputs = {
-                    'input': x,
-                    'input_lengths': x_len,
-                    'scales': scales,
-                    'sid': sid
-                }
-        t1 = time.time()
-        audio = np.squeeze(ort_sess.run(None, ort_inputs))
-        audio *= 32767.0 / max(0.01, np.max(np.abs(audio))) * 0.6
-        audio = np.clip(audio, -32767.0, 32767.0)
-        t2 = time.time()
-        spending_time = "推理时间："+str(t2-t1)+"s" 
-        print(spending_time)
-        bytes_wav = bytes()
-        wavfile.write(outdir + '/'+fileName,hps.data.sampling_rate, audio.astype(np.int16))
-     
 
 if __name__ == '__main__':
      str1="你好呀，我的名字是七奈，现在五点了，下午好，喵喵喵~喵喵~喵~~喵喵喵~"
     #  generated_speech(str1,'temp1.wav')
-     generated_speech2(str1,'temp1.wav')
+     generated_speech(str1,'temp1.wav')
