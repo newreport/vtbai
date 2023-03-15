f8d52acdd432b43b80e565c827e50d0de8ca2954
newreport <haruki959@qq.com>
2023-03-15 12:08:09 +0800

    .

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ mkdir output
 wget -O models/model.onnx  https://huggingface.co/Mahiruoshi/vits_onnx_model/resolve/main/model.onnx 
 wget -O models/config.json  https://huggingface.co/Mahiruoshi/vits_onnx_model/resolve/main/config.json
 
-pip install requirements.txt
+pip install -r requirements.txt
 git submodule update --init --recursive
+pip install -r blivedm/requirements.tx
